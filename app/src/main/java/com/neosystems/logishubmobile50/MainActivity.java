66f9c69dba59dc9f6e3edd584d11552409c0ba11
jwpyo <pyojaewan@naver.com>
2016-07-13 11:08:19 +0900

    ...

--- app/src/main/java/com/neosystems/logishubmobile50/MainActivity.java
+++ app/src/main/java/com/neosystems/logishubmobile50/MainActivity.java
@@ -2,6 +2,7 @@ package com.neosystems.logishubmobile50;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,9 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("aa", "onCreate: 11111111111111111111");
         System.out.println("We are going to use GIT as a version control system.");
-
-
-
     }
 }
