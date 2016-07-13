cc0859c19e2d0ab8062838934cc7460093680eb2
Daniel <ydkim@logishub.net>
2016-07-13 10:24:08 +0900

    commit test

---
+++ app/src/main/java/com/neosystems/logishubmobile50/MainActivity.java
@@ -0,0 +1,16 @@
+package com.neosystems.logishubmobile50;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("Commit Test");
+
+
+    }
+}
