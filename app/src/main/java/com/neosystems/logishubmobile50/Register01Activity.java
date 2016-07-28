eaefce8ed2104eba4f191c04e65a645bebe10831
Daniel <ydkim@logishub.net>
2016-07-28 18:15:38 +0900

    commit ^^

---
+++ app/src/main/java/com/neosystems/logishubmobile50/Register01Activity.java
@@ -0,0 +1,13 @@
+package com.neosystems.logishubmobile50;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Register01Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register01);
+    }
+}
