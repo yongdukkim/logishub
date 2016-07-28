563c70f9a4b4fbbc5d976a75efbff6dcdfac8820
jwpyo <pyojaewan@naver.com>
2016-07-28 09:30:14 +0900

    ...

--- app/src/main/java/com/neosystems/logishubmobile50/Common/CustomProgressDialog.java
+++ app/src/main/java/com/neosystems/logishubmobile50/Common/CustomProgressDialog.java
@@ -10,6 +10,6 @@ public class CustomProgressDialog extends Dialog{
     public CustomProgressDialog(Context context) {
         super(context);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.customdialog);
+        setContentView(R.layout.custom_dialog);
     }
 }
